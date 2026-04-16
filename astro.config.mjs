2d4f56b4d7d7ad98d09c15dea329b915ab731f43
Amitava Saha <amitava82@gmail.com>
2026-04-16 15:12:29 +0530

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 
 export default defineConfig({
-  site: 'https://docs.kloudmate.com',
+  site: 'https://kloudmate.github.io/docs/',
   integrations: [
     starlight({
       title: 'KloudMate Docs',
